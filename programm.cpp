6a75531fc1f7154cde8aa23ce9d8e076a4c5c78c
Evgeny <kkrutkov02@gmail.com>
2023-02-26 01:38:18 +0300

    Add printf and scanf f:a, b, c

--- programm.cpp
+++ programm.cpp
@@ -6,4 +6,10 @@ int main()
 {
 	setlocale(LC_ALL "Russian");
 	float a, b, c, Xn, Xk, dx, x, F;
+	printf("¬ведите значение a: \n");
+	scanf_s(" %f", &a);
+	printf("¬ведите значение b: \n");
+	scanf_s(" %f", &b);
+	printf("¬ведите значение c: \n");
+	scanf_s(" %f", &c);
 }
